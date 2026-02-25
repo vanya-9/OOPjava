a6a8a5f93f31fdd44d7b9bac3575f3db8f0a7e4b
I <i.sitnikov1@g.nsu.ru>
2026-02-25 20:54:08 +0700

    add logger

--- lab1/src/calculator/factory/CommandFactory.java
+++ lab1/src/calculator/factory/CommandFactory.java
@@ -1,15 +1,23 @@
 package calculator.factory;
 
 import java.io.InputStream;
-import java.util.*;
+import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 import calculator.command.Command; 
 public class CommandFactory {
     private Map<String, String> commands = new HashMap<>(); 
+    private static final Logger logger = Logger.getLogger(CommandFactory.class.getName());
 
     public CommandFactory(){
         try(InputStream input = CommandFactory.class.getResourceAsStream("commands.properties")){ 
             Properties properties = new Properties(); 
+            if(input==null){
+                logger.severe("File not found, properties");
+                throw new RuntimeException("Can not load properties");
+            }
             properties.load(input); 
 
             for(String key : properties.stringPropertyNames()){ 
@@ -17,18 +25,22 @@ public class CommandFactory {
             }
         }
         catch(Exception e){
-            throw new RuntimeException("Can not load properties");
+            logger.severe("Mistake working file properties");
         }
     }
 
     public Command create(String commandName){ 
         String className = commands.get(commandName);
-            if (className == null)
-                throw new RuntimeException("Unknow command"); 
+        if (className == null){
+            logger.severe("Command not found");
+            throw new RuntimeException("Unknow command"); 
+        }
+                
         try {
             Class<?> commandClass = Class.forName(className); 
             return (Command) commandClass.getDeclaredConstructor().newInstance(); 
-        } catch (Exception e) { 
+        } catch (Exception e) {
+            logger.severe("Mistake create command");
             throw new RuntimeException(e);
         }
     }
